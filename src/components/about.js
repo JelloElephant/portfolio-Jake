753779e9c9ef10fe0d9e4c64f7cc1bb09d1eafba
Jake Eik <eik995@hotmail.com>
2023-08-17 20:55:07 -0500

    Added basic files

---
+++ src/components/about.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function About() {
+    return (
+        <div className='flex-items-center'>
+            <div className='font-size: large'> 
+                <h1>About Me</h1>
+                <p>Im an avid model enthuesiast and I can make fullsatck apps!</p>
+            </div>
+        </div>
+    )
+}
+export default About
